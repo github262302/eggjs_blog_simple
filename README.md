f59f558784b0a4249ce7b7f96a2ffa092c909e8b
余 <2623021604@qq.com>
2022-08-05 03:19:27 +0800

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+# eggjs_blog_simple
+ 使用eggis的个人博客api
---
+++ app/controller/blog.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this;
+    ctx.body = 'hi, egg';
+  }
+  async show() {
+    const { ctx } = this;
+    ctx.body = 'hi, show' + ctx.params.id;
+  }
+  async create() {
+    const { ctx } = this;
+    ctx.body = 'hi, create';
+  }
+  async update() {
+    const { ctx } = this;
+    ctx.body = 'hi, update';
+  }
+  async destroy() {
+    const { ctx } = this;
+    ctx.body = 'hi, destroy';
+  }
+}
+
+module.exports = HomeController;
